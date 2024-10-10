30969727761976504e10ed25c14d684855a326fa
JessYan0913 <1141326491@qq.com>
2024-10-10 16:36:26 +0800

    feat: 增加数据库连接逻辑

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import storageConfiguration from '@/config/storage.configuration';
 import { StorageModule } from '@/storage/storage.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma/prisma.service';
+import { UserService } from './user/user.service';
+import { PostService } from './post/post.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true, load: [storageConfiguration] }), StorageModule.register()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, UserService, PostService],
 })
 export class AppModule {}
