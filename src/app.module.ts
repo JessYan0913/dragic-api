3b230fc6a33d1205a1dd7faeccb1ef1541e15d92
JessYan0913 <1141326491@qq.com>
2024-10-10 17:17:49 +0800

    refactor: 模块化业务逻辑

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,16 @@ import { ConfigModule } from '@nestjs/config';
 import { PostService } from './post/post.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [storageConfiguration] }),
     StorageModule.register(),
     PrismaModule,
+    UserModule,
+    PostModule,
   ],
   controllers: [AppController],
   providers: [AppService, UserService, PostService],
