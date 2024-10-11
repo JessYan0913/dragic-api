02d2c9cea4cb5582c9fa22fcd90e592f48df3b7a
JessYan0913 <1141326491@qq.com>
2024-10-11 17:24:22 +0800

    feat: 实现角色接口

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { RoleService } from './role.service';
 
 @Controller('role')
-export class RoleController {}
+export class RoleController {
+  constructor(private readonly roleService: RoleService) {}
+
+  @Get('/all')
+  async getAll() {
+    return await this.roleService.roles({});
+  }
+
+  @Post()
+  async create(@Body() data: { name: string }) {
+    return await this.roleService.createRole(data);
+  }
+
+  @Put('/:id')
+  async update(@Param('id') id: string, @Body() data: { name: string }) {
+    return await this.roleService.updateRole(
+      {
+        where: { id: Number(id) },
+      },
+      data,
+    );
+  }
+
+  @Delete('/:id')
+  async delete(@Param('id') id: string) {
+    return await this.roleService.deleteRole({ id: Number(id) });
+  }
+}
--- src/role/role.service.ts
+++ src/role/role.service.ts
@@ -1,4 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@pictode-api/prisma';
+import { Prisma, Role } from '@prisma/client';
 
 @Injectable()
-export class RoleService {}
+export class RoleService {
+  constructor(private prisma: PrismaService) {}
+
+  async roles(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.RoleWhereUniqueInput;
+    where?: Prisma.RoleWhereInput;
+    orderBy?: Prisma.RoleOrderByWithRelationInput;
+  }): Promise<Role[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prisma.role.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async createRole(data: Prisma.RoleCreateInput): Promise<Role> {
+    return this.prisma.role.create({
+      data,
+    });
+  }
+
+  async updateRole(
+    params: {
+      where: Prisma.RoleWhereUniqueInput;
+    },
+    data: Prisma.RoleUpdateInput,
+  ): Promise<Role> {
+    const { where } = params;
+    return this.prisma.role.update({
+      data,
+      where,
+    });
+  }
+
+  async deleteRole(where: Prisma.RoleWhereUniqueInput): Promise<Role> {
+    return this.prisma.role.delete({
+      where,
+    });
+  }
+}
